8bb533eeaf5573c26419aa52ba3303688a83cfb7
ting <bullian99@gmail.com>
2023-12-19 10:37:54 +0800

    support cast (VOD)

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
 yt-dlp plugin for `https://www.spooncast.net/jp/live/`
 
+把這個 repo 的檔案下載，放到正確的地方，[yt-dlp](https://github.com/yt-dlp/yt-dlp) 就能下載 spoon 的影片。
+
 這邊只負責把 spoon 的 m3u8 網址抓出來，剩下的事情 yt-dlp + ffmpeg 會處理。
 
 # Install
 
-ref https://github.com/yt-dlp/yt-dlp#installing-plugins
+目錄有很多種選擇，參考： https://github.com/yt-dlp/yt-dlp#installing-plugins
 
-## windows:
+## Git 安裝
+
+1. 首先你要有 Git
+2. `mkdir $HOME/.config/yt-dlp/plugins`
+3. `cd $HOME/.config/yt-dlp/plugins`
+4. `git clone https://github.com/ting1322/yt-dlp-spoon-plugin.git`
+
+未來升級 plugin 就普通的 `git pull --rebase`
+
+windows 的話目錄是 `C:\Users\<USER_NAME>\AppData\Roaming\yt-dlp-plugins\`
+在這邊 git clone。
+
+## windows 手動安裝:
+
+給前一段看不懂的人。
 
 1. 檔案總管網址打 `%APPDATA%`
 2. 下載檔案 [spoon.py](yt_dlp_plugins/extractor/spoon.py)
@@ -19,3 +35,4 @@ ref https://github.com/yt-dlp/yt-dlp#installing-plugins
 
 1. 觀察 `--verbose` 的輸出，有沒有抓到 `yt-dlp-plugins` 目錄
 2. 確定你有一層不漏，一字不改的建好目錄結構
+3. 確定你給的網址是 jp 版的 spoon。另外也有 kr 版，這邊不支援。
--- yt_dlp_plugins/extractor/spoon.py
+++ yt_dlp_plugins/extractor/spoon.py
@@ -1,6 +1,7 @@
 from yt_dlp.extractor.common import InfoExtractor
 from yt_dlp.utils import UserNotLive
 from yt_dlp.utils import traverse_obj
+from datetime import datetime
 
 class SpoonLiveIE(InfoExtractor):
     _WORKING = True
@@ -17,15 +18,11 @@ class SpoonLiveIE(InfoExtractor):
 
         current_live_id = traverse_obj(profile_js_data, ('results', 0, 'current_live_id'))
 
-        self.to_screen('current live id %s' % current_live_id)
-
         if not current_live_id:
             raise UserNotLive(video_id=uploader_id)
 
         live_info_url = 'https://jp-api.spooncast.net/lives/%s/' % current_live_id
 
-        self.to_screen('get info page %s' % live_info_url)
-        
         webpage = self._download_webpage(live_info_url, current_live_id)
 
         video_js_data = self._parse_json(webpage, current_live_id)
@@ -65,3 +62,46 @@ class SpoonLiveIE(InfoExtractor):
             **base_dict,
             **infodict,
         }
+
+class SpoonCastIE(InfoExtractor):
+    _WORKING = True
+    _VALID_URL = r'https://www.spooncast.net/jp/cast/(?P<id>\d+)'
+
+    def _real_extract(self, url):
+        self.to_screen('URL "%s" successfully captured' % url)
+        video_id = self._match_id(url)
+
+        if video_id is None:
+            self._error_or_warning('load video info fail')
+            
+        video_info_url = 'https://jp-api.spooncast.net/casts/%s/' % video_id
+
+        webpage = self._download_webpage(video_info_url, video_id)
+        video_js_data = self._parse_json(webpage, video_id)
+        img_url = traverse_obj(video_js_data, ('results', 0, 'img_url'))
+        voice_url = traverse_obj(video_js_data, ('results', 0, 'voice_url'))
+        nickname = traverse_obj(video_js_data, ('results', 0, 'author', 'nickname'))
+        title = traverse_obj(video_js_data, ('results', 0, 'title'))
+        description = traverse_obj(video_js_data, ('results', 0, 'welcome_message'))
+
+        # 2023-01-19T13:00:18.828274Z
+        created = datetime.strptime(
+            traverse_obj(video_js_data, ('results', 0, 'created')),
+            '%Y-%m-%dT%H:%M:%S.%fZ')
+
+        self.to_screen(created)
+
+        if voice_url is None:
+            self._error_or_warning('not found m4a url')
+        
+        return {
+            'id': str(video_id),
+            'title': title,
+            'description': description,
+            'formats': [{'url':voice_url, 'format_id': 'http-mp4'}],
+            'thumbnail': img_url,
+            'uploader': nickname,
+            'timestamp': created.timestamp(),
+            }
+
+            
