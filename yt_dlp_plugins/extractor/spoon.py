faa862dc522c2e1f16cb0a39d47582a068533d36
ting <bullian99@gmail.com>
2023-12-24 20:53:07 +0800

    fix: incorrect uploader

--- yt_dlp_plugins/extractor/spoon.py
+++ yt_dlp_plugins/extractor/spoon.py
@@ -9,14 +9,14 @@ class SpoonLiveIE(InfoExtractor):
 
     def _real_extract(self, url):
         self.to_screen('URL "%s" successfully captured' % url)
-        uploader_id = self._match_valid_url(url).groups()
+        uploader_id = self._match_id(url)
         profile_url = 'https://jp-api.spooncast.net/profiles/%s/meta/' % uploader_id
         self.to_screen(profile_url)
         webpage = self._download_webpage(profile_url, uploader_id)
 
         profile_js_data = self._parse_json(webpage, uploader_id)
 
-        current_live_id = traverse_obj(profile_js_data, ('results', 0, 'current_live_id'))
+        current_live_id = str(traverse_obj(profile_js_data, ('results', 0, 'current_live_id')))
 
         if not current_live_id:
             raise UserNotLive(video_id=uploader_id)
@@ -68,7 +68,6 @@ class SpoonCastIE(InfoExtractor):
     _VALID_URL = r'https://www.spooncast.net/jp/cast/(?P<id>\d+)'
 
     def _real_extract(self, url):
-        self.to_screen('URL "%s" successfully captured' % url)
         video_id = self._match_id(url)
 
         if video_id is None:
