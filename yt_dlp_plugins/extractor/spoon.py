f974b270f88066fa59c691d1344b3671e14139a9
ting <bullian99@gmail.com>
2024-02-17 10:49:00 +0800

    print more info for debugging
    1. dump json from user profile meta.
    2. live info URL.

--- yt_dlp_plugins/extractor/spoon.py
+++ yt_dlp_plugins/extractor/spoon.py
@@ -8,20 +8,28 @@ class SpoonLiveIE(InfoExtractor):
     _VALID_URL = r'https://www.spooncast.net/jp/live/@(?P<id>.+)'
 
     def _real_extract(self, url):
-        self.to_screen('URL "%s" successfully captured' % url)
+        self.to_screen('spoon url "%s" successfully captured by SpoonLiveIE' % url)
         uploader_id = self._match_id(url)
         profile_url = 'https://jp-api.spooncast.net/profiles/%s/meta/' % uploader_id
-        self.to_screen(profile_url)
+        self.to_screen('get live id from user profile url: "%s"' % profile_url)
         webpage = self._download_webpage(profile_url, uploader_id)
+
+        self.to_screen('dump profile meta json\n------\n"%s"\n------\n' % webpage)
 
         profile_js_data = self._parse_json(webpage, uploader_id)
 
-        current_live_id = str(traverse_obj(profile_js_data, ('results', 0, 'current_live_id')))
+        current_live_id = traverse_obj(profile_js_data, ('results', 0, 'current_live_id'))
+
+        self.to_screen('current live id: "%s"' % str(current_live_id))
 
         if not current_live_id:
             raise UserNotLive(video_id=uploader_id)
 
+        current_live_id = str(current_live_id) # avoid type warning
+
         live_info_url = 'https://jp-api.spooncast.net/lives/%s/' % current_live_id
+
+        self.to_screen('live info url: "%s"' % live_info_url)
 
         webpage = self._download_webpage(live_info_url, current_live_id)
 
@@ -37,6 +45,8 @@ class SpoonLiveIE(InfoExtractor):
 
         if not hls_url:
             raise UserNotLive(video_id=uploader_id)
+
+        self.to_screen('m3u8 url: "%s"' % hls_url)
 
         m3u8_url = hls_url
 
